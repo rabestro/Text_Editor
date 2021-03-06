d946eed97368de5f9e79180085cddfceaf6e9698
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-06 16:22:45 +0200

    Add images

--- Text Editor/task/src/editor/component/AppToolbar.java
+++ Text Editor/task/src/editor/component/AppToolbar.java
@@ -10,22 +10,22 @@ public class AppToolbar extends JPanel {
     private static final Logger log = Logger.getLogger(AppToolbar.class.getName());
 
     private final JTextField fileName = new JTextField(15);
-    private final JButton loadButton = new JButton("Load");
+    private final JButton openButton = new JButton(new ImageIcon("open.svg"));
     private final JButton saveButton = new JButton("Save");
 
     {
         fileName.setName("FilenameField");
-        loadButton.setName("LoadButton");
+        openButton.setName("LoadButton");
         saveButton.setName("SaveButton");
         setLayout(new FlowLayout(FlowLayout.LEFT));
         add(fileName);
-        add(loadButton);
+        add(openButton);
         add(saveButton);
     }
 
 
     public AppToolbar(final ActionListener loadText, final ActionListener saveText) {
-        loadButton.addActionListener(loadText);
+        openButton.addActionListener(loadText);
         saveButton.addActionListener(saveText);
 
     }
