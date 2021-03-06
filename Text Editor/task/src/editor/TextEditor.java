83bcf00c3642e4b50129eac15a6f067506289df2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-06 13:45:41 +0200

    Added class Toolbar

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -2,11 +2,6 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.logging.Logger;
 
 public class TextEditor extends JFrame {
@@ -31,65 +26,15 @@ public class TextEditor extends JFrame {
         scrollPane.setPreferredSize(new Dimension(250, 250));
     }
 
-    private final JTextField fileName = new JTextField(15);
-
-    {
-        fileName.setName("FilenameField");
-    }
-
-    private final JButton loadButton = new JButton("Load");
-
-    {
-        loadButton.setName("LoadButton");
-        loadButton.addActionListener(this::loadFile);
-    }
-
-    private final JButton saveButton = new JButton("Save");
-
-    {
-        saveButton.setName("SaveButton");
-        saveButton.addActionListener(this::saveFile);
-    }
-
-    private final JPanel topMenu = new JPanel();
-
-    {
-        topMenu.setLayout(new FlowLayout());
-        topMenu.add(fileName);
-        topMenu.add(saveButton);
-        topMenu.add(loadButton);
-    }
-
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 300);
         setTitle("The second stage");
 
-        add(topMenu, BorderLayout.NORTH);
+        add(new Toolbar(textArea::getText, textArea::setText), BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
 
         setVisible(true);
-    }
-
-    private void saveFile(final ActionEvent actionEvent) {
-        try {
-            final var filePath = Path.of(fileName.getText());
-            Files.writeString(filePath, textArea.getText(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-        } catch (IOException e) {
-            log.warning(e::getMessage);
-        }
-    }
-
-    private void loadFile(final ActionEvent actionEvent) {
-        try {
-            final var filePath = Path.of(fileName.getText());
-            textArea.setText(Files.readString(filePath));
-            Files.readString(filePath);
-        } catch (IOException e) {
-            textArea.setText("");
-            log.warning(e::getMessage);
-        }
-
     }
 
 }
---
+++ Text Editor/task/src/editor/Toolbar.java
@@ -0,0 +1,57 @@
+package editor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class Toolbar extends JPanel implements ActionListener {
+    private static final Logger log = Logger.getLogger(TextEditor.class.getName());
+
+    private final Supplier<String> textSupplier;
+    private final Consumer<String> textConsumer;
+    private final JTextField fileName = new JTextField(15);
+    private final JButton loadButton = new JButton("Load");
+    private final JButton saveButton = new JButton("Save");
+
+    {
+        fileName.setName("FilenameField");
+        loadButton.setName("LoadButton");
+        loadButton.addActionListener(this);
+        saveButton.setName("SaveButton");
+        saveButton.addActionListener(this);
+    }
+
+    Toolbar(final Supplier<String> textSupplier, final Consumer<String> textConsumer) {
+        this.textSupplier = textSupplier;
+        this.textConsumer = textConsumer;
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        add(fileName);
+        add(loadButton);
+        add(saveButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        final var filePath = Path.of(fileName.getText());
+
+        try {
+            if (event.getSource().equals(loadButton)) {
+                textConsumer.accept(Files.readString(filePath));
+            } else {
+                Files.writeString(filePath, textSupplier.get(), CREATE, WRITE, TRUNCATE_EXISTING);
+            }
+        } catch (IOException e) {
+            log.warning(e::getMessage);
+        }
+
+    }
+}
