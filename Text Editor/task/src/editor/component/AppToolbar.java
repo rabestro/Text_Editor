800f9a2881dabcd0d67fa973da92098bfd6e982a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-07 17:53:10 +0200

    Simplify code for AppToolbar

--- Text Editor/task/src/editor/component/AppToolbar.java
+++ Text Editor/task/src/editor/component/AppToolbar.java
@@ -8,53 +8,42 @@ import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.Insets;
 import java.util.Objects;
-import java.util.function.BiConsumer;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 public class AppToolbar extends JPanel {
     private static final Logger log = Logger.getLogger(AppToolbar.class.getName());
 
+    private final CommandListener listener;
     private final JTextField textPattern = new JTextField(15);
     private final JCheckBox useRegex = new JCheckBox("Use regex");
 
-    {
+    public AppToolbar(final CommandListener listener) {
+        this.listener = listener;
         textPattern.setName("SearchField");
         useRegex.setName("UseRegExCheckbox");
-    }
-
-    public AppToolbar(final CommandListener listener) {
-        final var openButton = addButton("OpenButton");
-        final var saveButton = addButton("SaveButton");
-        final var searchButton = addButton("StartSearchButton");
-        final var previousButton = addButton("PreviousMatchButton");
-        final var nextButton = addButton("NextMatchButton");
-
-        final BiConsumer<JButton, Command> addButton = (button, command) -> {
-            button.addActionListener(event ->
-                    listener.commandEventOccurred(new CommandEvent(this, command)));
-            add(button);
-        };
 
         setLayout(new FlowLayout(FlowLayout.LEFT));
-        addButton.accept(openButton, Command.OPEN);
-        addButton.accept(saveButton, Command.SAVE);
+        addButton("OpenButton", Command.OPEN);
+        addButton("SaveButton", Command.SAVE);
         add(textPattern);
-        addButton.accept(searchButton, Command.START_SEARCH);
-        addButton.accept(previousButton, Command.PREVIOUS);
-        addButton.accept(nextButton, Command.NEXT);
+        addButton("StartSearchButton", Command.START_SEARCH);
+        addButton("PreviousMatchButton", Command.PREVIOUS);
+        addButton("NextMatchButton", Command.NEXT);
         add(useRegex);
     }
 
-    private JButton addButton(final String name) {
+    private void addButton(final String name, final Command command) {
         Objects.requireNonNull(name);
         // The working directory should be set properly to load icons...
         final var url = "src/editor/images/" + name + ".gif";
         final var button = new JButton();
-//        button.setName(name);
+        button.setName(name);
         button.setMargin(new Insets(0, 0, 0, 0));
         button.setIcon(new ImageIcon(url, name));
-        return button;
+        button.addActionListener(event ->
+                listener.commandEventOccurred(new CommandEvent(this, command)));
+        add(button);
     }
 
     public Pattern getPattern() {
