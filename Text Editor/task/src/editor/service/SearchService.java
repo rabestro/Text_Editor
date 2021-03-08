4f77b99faab374ecec67a53a1387277d412585b2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-08 14:41:53 +0200

    Code optimization

--- Text Editor/task/src/editor/service/SearchService.java
+++ Text Editor/task/src/editor/service/SearchService.java
@@ -4,6 +4,7 @@ import editor.component.TextPane;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
 import java.util.logging.Logger;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -13,7 +14,7 @@ public class SearchService {
     private static final Logger log = Logger.getLogger(SearchService.class.getName());
 
     private final TextPane textArea;
-    private List<MatchResult> matchResultList;
+    private List<MatchResult> matches;
     private int index;
 
     public SearchService(final TextPane textArea) {
@@ -22,44 +23,32 @@ public class SearchService {
     }
 
     public void reset() {
-        matchResultList = Collections.emptyList();
+        matches = Collections.emptyList();
     }
 
     public void startSearch(final Pattern pattern) {
         log.entering(SearchService.class.getName(), "startSearch", pattern);
         final var matcher = pattern.matcher(textArea.getText());
-        matchResultList = matcher.results().collect(Collectors.toUnmodifiableList());
+        matches = matcher.results().collect(Collectors.toUnmodifiableList());
         index = -1;
         next();
-        log.exiting(SearchService.class.getName(), "startSearch", matchResultList.size());
+        log.exiting(SearchService.class.getName(), "startSearch", matches.size());
     }
 
     public void next() {
-        log.entering(SearchService.class.getName(), "next");
-        if (matchResultList.isEmpty()) {
-            return;
-        }
-        if (++index == matchResultList.size()) {
-            index = 0;
-        }
-        selectText(index);
-        log.exiting(SearchService.class.getName(), "next");
+        selectText(i -> (i + 1) % matches.size());
     }
 
     public void previous() {
-        log.entering(SearchService.class.getName(), "previous");
-        if (matchResultList.isEmpty()) {
-            return;
-        }
-        if (--index < 0) {
-            index = matchResultList.size() - 1;
-        }
-        selectText(index);
-        log.exiting(SearchService.class.getName(), "previous");
+        selectText(i -> (i + matches.size() - 1) % matches.size());
     }
 
-    private void selectText(final int index) {
-        final var result = matchResultList.get(index);
+    private void selectText(final IntUnaryOperator operator) {
+        if (matches.isEmpty()) {
+            return;
+        }
+        index = operator.applyAsInt(index);
+        final var result = matches.get(index);
         textArea.highlightMatch(result.start(), result.end());
     }
 
